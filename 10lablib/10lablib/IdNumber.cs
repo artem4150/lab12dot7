10cf1685d685326b790a4491e338802349bef70a
Артём Горовой <artemiygo9@gmail.com>
2024-04-24 01:27:00 +0500

    правки 1 часть

---
+++ 10lablib/10lablib/IdNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class IdNumber
+    {
+        public int number;
+        public IdNumber(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Error");
+            }
+
+            this.number = number;
+        }
+
+        public override string ToString()
+        {
+            return number.ToString();
+        }
+        
+        public int ToInt()
+        {
+            return number;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is IdNumber n)
+            {
+                return this.number == n.number;
+            }
+            return false;
+        }
+        
+    }
+}
+
